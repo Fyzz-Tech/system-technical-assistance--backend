012eec9cb1f0237c61b8bb03aaf21af3de0b9074
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-18 16:22:45 -0300

    chore(product): adiciona MapStruct para mapeamento automático de DTOs

---
+++ src/main/java/com/technical_assistance/project/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.technical_assistance.project.mapper;
+
+import com.technical_assistance.project.dtos.product.ProductRequestDTO;
+import com.technical_assistance.project.dtos.product.ProductResponseDTO;
+import com.technical_assistance.project.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    Product toEntity(ProductRequestDTO productRequestDTO);
+    ProductResponseDTO toResponse(Product product);
+    void updateProductFromDTO(ProductRequestDTO dto, @MappingTarget Product entity);
+}
