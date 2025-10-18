b96b4f5bcaae9f8f4c7c4a6f3b2d430fcb9cf164
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-18 16:21:35 -0300

    feat(product): adiciona ProductRepository para persistência com MongoRepository

---
+++ src/main/java/com/technical_assistance/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.technical_assistance.project.repositories;
+
+import com.technical_assistance.project.entities.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
