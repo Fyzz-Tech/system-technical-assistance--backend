58449d33a1774f6fc2465a966a50f0e7048ee9b2
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-18 18:30:23 -0300

    chore: adiciona enums Category e Status para padronização de produtos

---
+++ src/main/java/com/technical_assistance/project/enuns/Category.java
@@ -0,0 +1,13 @@
+package com.technical_assistance.project.enuns;
+
+public enum Category {
+
+    CAPA,
+    PELICULA,
+    CARREGADOR,
+    FONE,
+    BATERIA,
+    ADAPTADOR,
+    CABO,
+    SUPORTE
+}
---
+++ src/main/java/com/technical_assistance/project/enuns/Status.java
@@ -0,0 +1,6 @@
+package com.technical_assistance.project.enuns;
+
+public enum Status {
+    ATIVO,
+    INATIVO;
+}
