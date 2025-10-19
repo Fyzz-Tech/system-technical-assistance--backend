e9bb7e96d9830fdb096c25553a5d0130e64dfa11
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-19 10:47:12 -0300

    chore(user): remove construtor do UserResponseDTO não utilizado

--- src/main/java/com/technical_assistance/project/dtos/user/UserResponseDTO.java
+++ src/main/java/com/technical_assistance/project/dtos/user/UserResponseDTO.java
@@ -1,14 +1,5 @@
 package com.technical_assistance.project.dtos.user;
 
-import com.technical_assistance.project.entities.User;
 import com.technical_assistance.project.enuns.RoleUser;
 
-public record UserResponseDTO(String name, String email, RoleUser role) {
-
-    public UserResponseDTO (User user) {
-        this (
-                user.getName(),
-                user.getEmail(),
-                user.getRole());
-    }
-}
+public record UserResponseDTO(String name, String email, RoleUser role) {}
