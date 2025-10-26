31f086a618dbab1b7f6a42dae1e785938a96deb7
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-26 14:39:00 -0300

    feat(client): adiciona ClientController com endpoints de findAll, create, update e delete

---
+++ src/main/java/com/technical_assistance/project/controllers/ClientController.java
@@ -0,0 +1,54 @@
+package com.technical_assistance.project.controllers;
+
+import com.technical_assistance.project.dtos.client.ClientRequestDTO;
+import com.technical_assistance.project.dtos.client.ClientResponseDTO;
+import com.technical_assistance.project.entities.Client;
+import com.technical_assistance.project.mapper.ClientMapper;
+import com.technical_assistance.project.services.ClientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+@RequiredArgsConstructor
+public class ClientController {
+
+    private final ClientService service;
+    private final ClientMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<List<ClientResponseDTO>> findAll(){
+        List<ClientResponseDTO> clients = service.findAll();
+        return ResponseEntity.ok(clients);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> create(@RequestBody @Valid ClientRequestDTO dto){
+        Client newClient = service.create(dto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newClient.getId())
+                .toUri();
+        ClientResponseDTO response = mapper.toResponseDTO(newClient);
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientResponseDTO> update(@PathVariable String id, @RequestBody @Valid ClientRequestDTO dto){
+        Client current = service.update(id, dto);
+        ClientResponseDTO response = mapper.toResponseDTO(current);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
