133cebcf60fa1adf8eeaf4193c816cc04b1106a9
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-31 22:21:51 -0300

    chore: adiciona enuns PaymentMethod e StatusSale para padronização de resposta

---
+++ src/main/java/com/technical_assistance/project/enuns/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.technical_assistance.project.enuns;
+
+public enum PaymentMethod {
+    DINHEIRO,
+    PIX,
+    CARTAO_DE_CREDITO,
+    CARTAO_DE_DEBITO;
+}
---
+++ src/main/java/com/technical_assistance/project/enuns/StatusSale.java
@@ -0,0 +1,7 @@
+package com.technical_assistance.project.enuns;
+
+public enum StatusSale {
+    PAGO,
+    CANCELADO,
+    AGUARDANDO_PAGAMENTO;
+}
