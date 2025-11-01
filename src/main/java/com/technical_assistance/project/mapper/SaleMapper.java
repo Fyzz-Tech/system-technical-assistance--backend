caf716fc11db6489f0ed8d144f0e5f3c225b0c83
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-31 22:26:39 -0300

    chore(sale): adiciona MapStruct para mapeamento automático de DTOs

---
+++ src/main/java/com/technical_assistance/project/mapper/SaleMapper.java
@@ -0,0 +1,15 @@
+package com.technical_assistance.project.mapper;
+
+import com.technical_assistance.project.dtos.sale.SaleRequestDTO;
+import com.technical_assistance.project.dtos.sale.SaleResponseDTO;
+import com.technical_assistance.project.entities.Sale;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface SaleMapper {
+
+    Sale toEntity(SaleRequestDTO saleRequestDTO);
+    SaleResponseDTO toResponseDTO(Sale sale);
+    void updateSaleFromDTO(SaleRequestDTO dto, @MappingTarget Sale entity);
+}
