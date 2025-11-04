94330d3e09b966586f2e6933d32487ed2f4ce871
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-11-04 08:28:14 -0300

    refactor(product): ajuste de getters/setters, adição de atributo e construtor personalizado

--- src/main/java/com/technical_assistance/project/entities/Product.java
+++ src/main/java/com/technical_assistance/project/entities/Product.java
@@ -2,26 +2,30 @@ package com.technical_assistance.project.entities;
 
 import com.technical_assistance.project.enuns.Category;
 import com.technical_assistance.project.enuns.Status;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
 
     @Id
     private String id;
-
     private String name;
     private Status status;
     private Category category;
-    private Double unitPrice;
-    private Integer quantityStock;
+    private Double priceForSale;
+    private Double priceAtCost;
+
+    public Product(String name, Status status, Category category, Double priceForSale, Double priceAtCost) {
+        this.name = name;
+        this.status = status;
+        this.category = category;
+        this.priceForSale = priceForSale;
+        this.priceAtCost = priceAtCost;
+    }
+
 }
