e76aae6375748ea2893639fea830c0f5c3366158
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-11-04 08:31:42 -0300

    refactor(mappers): ajuste nos métodos de conversão e update de DTOs

--- src/main/java/com/technical_assistance/project/mapper/ProductMapper.java
+++ src/main/java/com/technical_assistance/project/mapper/ProductMapper.java
@@ -1,7 +1,8 @@
 package com.technical_assistance.project.mapper;
 
 import com.technical_assistance.project.dtos.product.ProductRequestDTO;
-import com.technical_assistance.project.dtos.product.ProductResponseDTO;
+import com.technical_assistance.project.dtos.product.ProductRequestUpdateDTO;
+import com.technical_assistance.project.dtos.product.ProductStockDTO;
 import com.technical_assistance.project.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -10,6 +11,6 @@ import org.mapstruct.MappingTarget;
 public interface ProductMapper {
 
     Product toEntity(ProductRequestDTO productRequestDTO);
-    ProductResponseDTO toResponseDTO(Product product);
-    void updateProductFromDTO(ProductRequestDTO dto, @MappingTarget Product entity);
+    ProductStockDTO toResponseDTO(Product product);
+    void updateProductFromDTO(ProductRequestUpdateDTO dto, @MappingTarget Product entity);
 }
--- src/main/java/com/technical_assistance/project/mapper/SaleMapper.java
+++ src/main/java/com/technical_assistance/project/mapper/SaleMapper.java
@@ -4,12 +4,12 @@ import com.technical_assistance.project.dtos.sale.SaleRequestDTO;
 import com.technical_assistance.project.dtos.sale.SaleResponseDTO;
 import com.technical_assistance.project.entities.Sale;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SaleMapper {
 
     Sale toEntity(SaleRequestDTO saleRequestDTO);
     SaleResponseDTO toResponseDTO(Sale sale);
-    void updateSaleFromDTO(SaleRequestDTO dto, @MappingTarget Sale entity);
+
 }
